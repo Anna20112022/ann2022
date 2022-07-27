995a9063c2844ba54aef79e9e48bd52e596911fd
Anna20112022 <110093638+Anna20112022@users.noreply.github.com>
2022-07-27 14:22:21 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+
+var b1 = document.getElementById("b1")
+
+var h11 = document.getElementById("h11")
+
+b1.onclick = function() {
+    h11.style.color = "red";
+}
+
+var b2 = document.getElementById("b2")
+b2.onclick = function() {
+    h11.style.color = "green";
+}
+var b3 = document.getElementById("b3")
+b3.onclick = function() {
+    h11.style.color = "blue";
+}
+var h11 = document.getElementById("h11")
+
+b1.onclick = function() {
+    h11.style.color = "red";
+}
+
+var num = 38 % 5;
+console.log(num)
+console.log("Hello World!")
+
+
+
